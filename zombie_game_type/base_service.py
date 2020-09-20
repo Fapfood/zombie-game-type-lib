ce07ad791807fb3a502c5b34f7edf7b1e4aa990f
matr@grapeup.com <matr@grapeup.com>
2020-09-20 02:17:07 +0200

    base service change

--- zombie_game_type/base_service.py
+++ zombie_game_type/base_service.py
@@ -1,4 +1,5 @@
 import os
+from random import sample
 
 from yaml import FullLoader, load
 
@@ -6,15 +7,30 @@ SCRIPT_DIR = os.path.dirname(__file__)
 
 
 class BaseService:
-    def __init__(self, data_file_name):
+    def __init__(self, name):
         self.data = {}
-        file_name = '{}/data/{}'.format(SCRIPT_DIR, data_file_name)
+
+        file_name = '{}/data/{}.yml'.format(SCRIPT_DIR, name)
         with open(file_name, encoding='utf8') as f:
             lis = load(f.read(), Loader=FullLoader)['list']
         self.get_or_create_bulk_from_yaml(lis)
 
+        dir_name = '{}/data/{}'.format(SCRIPT_DIR, name)
+        for _, _, file_names in os.walk(dir_name):
+            for file_name in file_names:
+                file_name = '{}/data/{}/{}'.format(SCRIPT_DIR, name, file_name)
+                with open(file_name, encoding='utf8') as f:
+                    el = load(f.read(), Loader=FullLoader)
+                self.get_or_create_from_yaml(el)
+
     def get_by_name(self, name: str):
         return self.data.get(name)
+
+    def get_random(self):
+        return sample(self.get_all(), 1)[0]
+
+    def get_random_name(self):
+        return sample(self.get_all_names(), 1)[0]
 
     def get_all(self):
         return frozenset(self.data.values())
